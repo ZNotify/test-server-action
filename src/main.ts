b5810cbb33b9151890baeb7bfc882ceb84981faa
Zxilly <zxilly@outlook.com>
2022-10-31 18:11:40 +0000

    refactor: use nodejs

---
+++ src/main.ts
@@ -0,0 +1,66 @@
+import * as core from '@actions/core';
+import * as artifact from '@actions/artifact';
+import * as fs from 'fs';
+import * as exec from '@actions/exec';
+import { spawn } from 'child_process';
+
+const artifactClient = artifact.create();
+
+type OS = 'Linux' | 'macOS' | 'Windows';
+
+function getFilename(url: string) {
+    return url.split('/').pop();
+}
+
+const assetMap: { [key: string]: string } = {
+    "Linux": "https://github.com/ZNotify/server/releases/download/latest/server-linux",
+    "Windows": "https://github.com/ZNotify/server/releases/download/latest/server-windows.exe",
+    "macOS": "https://github.com/ZNotify/server/releases/download/latest/server-macos"
+}
+
+const runnerOS = process.env['RUNNER_OS'];
+
+async function run() {
+    const tempPath = await fs.promises.mkdtemp('server');
+    const da = await downloadArifact(tempPath);
+    if (!da) {
+        await downloadRelease(tempPath);
+    }
+    await execBinary(tempPath);
+}
+
+async function execBinary(path: string) {
+    const filename = getFilename(assetMap[runnerOS as OS]);
+    const execPath = path + '/' + filename;
+    spawn(execPath, ["--test"], {
+        detached: true,
+    })
+}
+
+async function downloadRelease(path: string) {
+    const downloadURL = assetMap[runnerOS as OS];
+    const filename = getFilename(downloadURL);
+    const downloadPath = path + '/' + filename;
+    const resp = await fetch(downloadURL);
+    if (!resp.ok) {
+        core.setFailed('Failed to download release');
+        throw new Error(`unexpected response ${resp.statusText}`);
+    }
+    const buffer = await resp.arrayBuffer();
+    await fs.promises.writeFile(downloadPath, Buffer.from(buffer));
+}
+
+async function downloadArifact(path: string): Promise<Boolean> {
+    try {
+        artifactClient.downloadArtifact('server', path, {
+            createArtifactFolder: false
+        });
+        return true;
+    } catch (error) {
+        core.info('Artifact may not exist, downloading from release');
+        return false;
+    }
+}
+
+run().catch(error => core.setFailed(error.message));
+
