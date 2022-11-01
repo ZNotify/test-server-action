5765798e8c42eeda0ad5584c5ea42948b085bbad
Zxilly <zxilly@outlook.com>
2022-11-01 07:07:38 +0000

    fix: update log

--- src/post.ts
+++ src/post.ts
@@ -1,22 +1,17 @@
 import * as core from '@actions/core';
 import * as fs from 'fs';
-import * as exec from '@actions/exec';
-
 
 async function run() {
     const path = core.getState('tempPath');
-    const pid = core.getState('pid');
-
-    if (pid) {
-        core.info(`Killing process with PID ${pid}`);
-        await exec.exec('kill', [pid]);
-    }
 
     if (path) {
         // read log
         const logPath = 'data/app.log';
         const log = await fs.promises.readFile(logPath, 'utf-8');
+        core.summary.addHeading('Server log');
+        core.summary.addBreak();
         core.summary.addCodeBlock(log, 'log');
+        core.summary.write();
     }
 }
 
